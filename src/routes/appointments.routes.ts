b69a00e75bced138913fc49eed1e59bd4a6bf041
Andre <andredelima92@gmail.com>
2021-05-19 22:08:31 -0300

    feature: method all and DTO

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,6 +5,12 @@ import AppointmentsRepository from "../repositories/AppointmentsRepository";
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get("/", (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
 
@@ -19,13 +25,12 @@ appointmentsRouter.post("/", (request, response) => {
       .json({ message: "This appointment is already booked" });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
-
-// appointmentsRouter.get("/", (request, response) => {
-//   return response.json(appointments);
-// });
 
 export default appointmentsRouter;
